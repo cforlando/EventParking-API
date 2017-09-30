1f25d7902656051b99e47577976d4c1cedf2df45
Tom Marks <tmarks@unikey.com>
2017-09-30 13:27:16 -0400

    ready for heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.port = 8080;
+config.port = 5000;
 config.dataRequestInterval = 1209600
 
 module.exports = config;
--- server.js
+++ server.js
@@ -4,28 +4,10 @@ var express = require('express');
 var config = require('./config');
 var timer = require('timers');
 var fs = require('fs');
-var http = require('http');
-var https = require('https');
-
-// Load certs
-var privateKey  = fs.readFileSync('server/privkey.pem', 'utf8');
-var certificate = fs.readFileSync('server/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
 
 var app = express();
 const port = config.port;
 const dataRequestInterval = config.dataRequestInterval;
-// your express configuration here
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080, (request, response) => {
-    console.log("http server started");
-    
-});
-httpsServer.listen(8443, (request, response) => {
-    console.log("https server started");
-});
 
 function requestData(callback) {
     callback("some data");
@@ -77,3 +59,6 @@ app.post('/action', (request, response, body) => {
     response.send(actionResponse);
 });
 
+app.listen(port, (error) => {
+    console.log("Something gone broke: ", error);
+});

9a21ecb1d7a0c5faea0ba85a2018f360e5522084
Tom Marks <tmarks@unikey.com>
2017-09-30 14:10:01 -0400

    ready for heroku

--- server.js
+++ server.js
@@ -28,29 +28,53 @@ function startWatcher() {
     }, dataRequestInterval); // This will fire every two weeks.
 }
 
+function eventParking(date) {
+    var contents = fileSystem.readFileSync("dates.json");
+    var json = JSON.parse(contents);
+
+    var month = date.month;
+    var day = date.date;
+    var year = date.year;
+
+    console.log("Date: ",date);
+    console.log("JSON: ",json);
+
+    if (year == json.year) {
+        console.log("Year matches");
+        if (month == json.month) {
+            console.log("Month matches");
+            if (json.days.indexOf(day) > -1) {
+                console.log("Day matches");
+                return true
+            } else {
+                console.log("Day doesn't match");
+            }
+        } else {
+            console.log("Month doesn't match");
+        }
+    } else {
+        console.log("Year doesn't match");
+    }
+
+    return false
+}
+
 app.get('/', (request, response) => {
     response.send('This is not the end point you are looking for.');
 });
 
 app.post('/events', (request, response) => {
     // Build some json body.
-    var contents = fileSystem.readFileSync("dates.json");
-    var json = JSON.parse(contents);
+    
+    var success = eventParking(Date.now());
 
-    var today = Date.now();
-    var month = 10;//today.month;
-    var day = 31;//today.date;
-    var year = today.year;
-
-    if (year == json.year) {
-        if (month == json.month) {
-            if (json.days.indexOf(day) > -1) {
-                response.send("Yes, there is event parking.");
-            }
-        }
+    if (success) {
+        response.send("Yes, there is event parking.");
+    } else {
+        response.send("No, there isn't any event parking.");
     }
 
-    response.send("No, there isn't any event parking.");
+    
 });
 
 app.post('/action', (request, response, body) => {

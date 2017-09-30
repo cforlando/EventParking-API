75088a855c9abb41d3230b818454db0af3389366
Tom Marks <tmarks@unikey.com>
2017-09-30 11:27:02 -0400

    basic endpoints set up

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 var config = {};
 
+config.port = 8080;
+config.dataRequestInterval = 1209600
+
 module.exports = config;
--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-request = require('request')
-express = require('express')
-config = require('./config')
+request = require('request');
+express = require('express');
+config = require('./config');
+timer = require('timers');
 
 var app = express();
+const port = config.port;
+const dataRequestInterval = config.dataRequestInterval;
+
+function requestData(callback) {
+    callback("some data");
+}
+
+function updateDatabase(data) {
+    var minimumData = Date.now()
+    // Add new data
+    // remove old data
+}
+
+function watcher() {
+    // Loop to periodically request data
+    setInterval(() => {
+        requestData((data) => {
+            updateDatabase(data);
+        });
+    }, dataRequestInterval);
+}
 
 app.get('/', (request, response) => {
+    response.send('This is not the end point you are looking for.');
+});
 
-}
+app.get('/events', (request, response) => {
+    // Build some json body.
+    var events = {
+        "Pine St" : {
+            "start" : "2017-10-06T17:30:00",
+            "end" : "2017-10-07T00:30:00",
+            "cost" : 11
+        }
+    }
+    response.send(events);
+});
+
+app.post('/events', (request, response, body) => {
+
+});
+
+app.listen(port, (error) => {
+    if (error) {
+        return console.log('something really bad happened. There is fire, someone soiled themself... ', error);
+    }
+
+    console.log(`server is listening on ${port}`);
+});

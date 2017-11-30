b7237a385270a72a6726dbf13eeb0f17ecba8892
Jared Ashcraft <jaredmashcraft@gmail.com>
2017-11-30 16:50:05 -0500

    adds CORS middleware

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var config = require('./config');
 var timer = require('timers');
 var fileSystem = require('fs');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 

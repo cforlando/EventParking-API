ed8d7c6d52014b3d168a905aa4c65cb80bece9b0
travis-konarik <travis_konarik@outlook.com>
2018-04-06 21:19:41 -0400

    Updated event to handle month and day requests via api

--- routes/event.js
+++ routes/event.js
@@ -23,11 +23,33 @@ router.use('/:year/:month/:day', async (req, res) => {
     const nextDate = getNextDate(year, month, day);
     const sql = "SELECT * FROM events WHERE (start_date BETWEEN '" + date + "' AND '" + nextDate + "') OR (end_date BETWEEN '" + date + "' AND '" + nextDate + "') OR ('" + date + "' BETWEEN start_date AND end_date);";
 
-    logger.info("Requested: " + date);
+    logger.info("Requested Day: " + date);
     logger.info("SQL: " + sql);
     const { rows } = await db.query(sql);
     res.send(rows);
 });
+
+router.use('/:year/:month', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    const year = req.params.year;
+    const month = req.params.month;
+    const date = year + '/' + month + '/01';
+    const endOfMonth = getEndOfMonth(year, month);
+    const sql = "SELECT * FROM events WHERE (start_date BETWEEN '" + date + "' AND '" + endOfMonth + "') OR (end_date BETWEEN '" + date + "' AND '" + endOfMonth + "') OR ('" + date + "' BETWEEN start_date AND end_date);";
+
+    logger.info("Requested Month: " + date);
+    logger.info("SQL: " + sql);
+    const { rows } = await db.query(sql);
+    res.send(rows);
+});
+
+function getEndOfMonth(year, month) {
+    const end = new Date(year, month, 0);
+
+    return end.getFullYear() + '/' + (end.getMonth() + 1) +'/' +end.getDate();
+}
 
 function getNextDate(year, month, day) {
     let monthIndex = parseInt(month) - 1;

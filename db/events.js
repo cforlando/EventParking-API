3a56ad00b1b856cf0ed5f3de5466a4fdf2049bdb
travis-konarik <travis_konarik@outlook.com>
2018-04-07 15:31:53 -0400

    Added testing for db/events.js

--- db/events.js
+++ db/events.js
@@ -2,33 +2,38 @@ const db = require('../db');
 const logger = require('heroku-logger');
 
 module.exports = {
-    addEvents: async function addEvents(events) {
-        logger.info('Run Query Started');
-        for (let k = 0; k < events.length; ++k) {
-            let sql = 'SELECT insertevent(\'';
-            sql += events[k]['venue'].split("'").join("''") + '\', \'';
-            sql += events[k]['event_id'].split("'").join("''") + '\', \'';
-            sql += events[k]['title'].split("'").join("''") + '\', \'';
-            sql += events[k]['description'].split("'").join("''") + '\', \'';
-            sql += events[k]['start_date'].split("'").join("''") + '\', \'';
-            sql += events[k]['end_date'].split("'").join("''") + '\', \'';
-            sql += events[k]['category'].split("'").join("''") + '\', \'';
-            sql += events[k]['promotions'].split("'").join("''") + '\', \'';
-            sql += events[k]['url'].split("'").join("''") + '\', \'';
-            sql += events[k]['image_url'].split("'").join("''") + '\');';
-
-            logger.info('Sql == ' + sql);
-
-            await
-            db.query(sql).then(function (res) {
-                logger.info('Added: ' + events[k]['event_id'] + ' :: ' + events[k]['title']);
-            })
-                .catch(function (err) {
-                    logger.error("Could Not Add: " + events[k]['event_id'] + ' :: ' + events[k]['title']);
-                });
-        }
-    }
+    addEvents: addEvents,
+    getEventInsertFunction: getEventInsertFunction,
 };
 
+async function addEvents(events) {
+    logger.info('Run Query Started');
+    for (let k = 0; k < events.length; ++k) {
+        let sql = getEventInsertFunction(events, k);
 
-// https://www.amwaycenter.com//events/calendar/2018/5
+        logger.info('Sql == ' + sql);
+
+        await db.query(sql).then(function (res) {
+            logger.info('Added: ' + events[k]['event_id'] + ' :: ' + events[k]['title']);
+        }).catch(function (err) {
+            logger.error("Could Not Add: " + events[k]['event_id'] + ' :: ' + events[k]['title']);
+        });
+    }
+    return 'Done';
+}
+
+function getEventInsertFunction(events, k) {
+    let sql = 'SELECT insertevent(\'';
+    sql += events[k]['venue'].split("'").join("''") + '\', \'';
+    sql += events[k]['event_id'].split("'").join("''") + '\', \'';
+    sql += events[k]['title'].split("'").join("''") + '\', \'';
+    sql += events[k]['description'].split("'").join("''") + '\', \'';
+    sql += events[k]['start_date'].split("'").join("''") + '\', \'';
+    sql += events[k]['end_date'].split("'").join("''") + '\', \'';
+    sql += events[k]['category'].split("'").join("''") + '\', \'';
+    sql += events[k]['promotions'].split("'").join("''") + '\', \'';
+    sql += events[k]['url'].split("'").join("''") + '\', \'';
+    sql += events[k]['image_url'].split("'").join("''") + '\');';
+
+    return sql;
+}
---
+++ test/routes-index-test.js
@@ -0,0 +1,37 @@
+let assert = require('assert');
+const index = require('../routes/index');
+const event = require('../routes/event');
+const calendar = require('../routes/calendar');
+const root = require('../routes/root');
+
+class mockApp {
+}
+
+// Mock Functions
+function mockUse(location, object) {
+    switch (location) {
+        case '/event':
+            assert.equal(object, event, 'event route');
+            break;
+        case '/calendar':
+            assert.equal(object, calendar, 'calendar route');
+            break;
+        case '/*':
+            assert.equal(object, root, 'root route');
+            break;
+        default:
+            throw 'Location Not Found';
+    }
+}
+
+// Tests
+describe('route-index', function () {
+
+    // Test #1 getEventInsertFunction
+    it('check for routes', function () {
+        const app = new mockApp();
+        app.use = mockUse;
+
+        index(app);
+    });
+});

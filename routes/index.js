3d2153b10bceea7e9eb1d09a06a4a1d1fd0690bb
travis-konarik <travis_konarik@outlook.com>
2018-04-09 22:51:03 -0400

    Fixed Router.use() requires a middleware function but got a Object

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const calendar = require('./calendar');
 const root = require('./root');
 
 module.exports = (app) => {
-    app.use('/event', event);
+    app.use('/event', event.router);
     app.use('/calendar', calendar);
     app.use('/*', root);
 };
